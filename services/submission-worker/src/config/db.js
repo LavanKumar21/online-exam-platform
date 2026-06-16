c1066d7208f03289dca883b93718d45e4433070b
LavanKumar21 <mekalalavankumar@gmail.com>
2026-06-17 01:37:18 +0530

    update the db config file in submission worker service

--- services/submission-worker/src/config/db.js
+++ services/submission-worker/src/config/db.js
@@ -4,15 +4,15 @@
 const { Pool}=require("pg")
 
 const pool=new Pool({
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT,
-    // connectionString: process.env.DATABASE_URL,
-    //  ssl: {
-    //     rejectUnauthorized: false
-    // },
+    // user:process.env.DB_USER,
+    // host:process.env.DB_HOST,
+    // database:process.env.DB_NAME,
+    // password:process.env.DB_PASSWORD,
+    // port:process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+     ssl: {
+        rejectUnauthorized: false
+    },
     max:20,
     idleTimeoutMillis:30000,
     connectionTimeoutMillis:2000
